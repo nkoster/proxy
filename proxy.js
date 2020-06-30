1a48cecf6b035e3bff02855a9a604c8da29b56d1
Niels Koster <n.koster@portavita.eu>
2020-06-30 20:26:48 +0200

    Cleanup

--- proxy.js
+++ proxy.js
@@ -1,23 +1,23 @@
-var net = require('net')
-
-// process.on('uncaughtException', function(err) {
-//     console.log('Caught exception: ' + err)
-// })
-
-var addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/
-
-var addr = {
+const net = require('net')
+const addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/
+const addr = {
     from: addrRegex.exec(process.argv[2]),
     to: addrRegex.exec(process.argv[3])
-};
+}
+
+process.on('uncaughtException', err => {
+    console.log('Caught exception: ' + err.message)
+})
 
 if (!addr.from || !addr.to) {
     console.log('Usage: <from> <to>')
     return
 }
 
-net.createServer(function(from) {
-    var to = net.createConnection({ host: addr.to[2], port: addr.to[3] })
+net.createServer(from => {
+    const to = net.createConnection({ host: addr.to[2], port: addr.to[3] })
     from.on('error', err => console.log('from error', err.message)).pipe(to)
-    to.on('error', err => console.log('from error', err.message)).pipe(from)
-}).listen(addr.from[3], addr.from[2]);
+    to.on('error', err => console.log('to error', err.message)).pipe(from)
+}).listen(addr.from[3], addr.from[2])
+
+console.log('Proxy from', addr.from['input'], 'to', addr.to['input'])
